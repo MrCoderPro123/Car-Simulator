5ede04598246f5a453eaa1ec8748a184a2021a68
MrCoderPro123 <ahmedakbarkhi23@gmail.com>
2026-03-27 18:34:15 +0500

    The First EVER Version:
    Engine RPM Functionality Added.

---
+++ Scripts/engine.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+@export var idle_rpm := 600
+@export var max_rpm := 7000
+@export var accelaration_speed := 3
+@export var deccelaration_speed := 1
+
+var throttle := 0.0
+var rpm := 0
+var target_rpm := 0
+var actual_smoothing_speed := 0
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("throttle_up"):
+		throttle += 0.1
+		actual_smoothing_speed = accelaration_speed
+	else:
+		throttle -= 0.1
+		actual_smoothing_speed = deccelaration_speed
+	
+	if throttle < 0:
+		throttle = 0
+	if throttle > 1:
+		throttle = 1
+	
+	target_rpm = idle_rpm + (max_rpm - idle_rpm) * throttle
+	
+	rpm = lerp(rpm, target_rpm, actual_smoothing_speed * delta)
+	rpm = clamp(rpm, idle_rpm, max_rpm)
+	
+	print(rpm)
+	
